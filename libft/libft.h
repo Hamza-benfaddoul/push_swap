5537aa060220cb5dd7d861e961d1e03c8ff860f5
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-01-28 10:19:54 +0100

    update make add libift

--- libft/libft.h
+++ libft/libft.h
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
+# include <stdint.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
